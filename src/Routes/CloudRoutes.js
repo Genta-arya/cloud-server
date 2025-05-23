a9a6838bfb7a2c9caf4884c8047cbe0f42b5fb41
Genta-arya <mgentaarya@gmail.com>
2025-05-23 20:00:49 +0700

    rewrite api

--- src/Routes/CloudRoutes.js
+++ src/Routes/CloudRoutes.js
@@ -7,7 +7,7 @@ import { handleDelete, handleUpload } from "../Controller/Cloud.controller.js";
 
 export const Routes = express.Router();
 // handle keduanya: 'file' (single) dan 'file[]' (array)
-Routes.post('/uploads', upload.fields([
+Routes.post('/uploads.php', upload.fields([
   { name: 'file', maxCount: 1 },
   { name: 'file[]', maxCount: 10 },
 ]), handleUpload);
